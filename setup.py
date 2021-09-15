ba5d892f32cf6675c576beeb1e89c6c20cfcd523
Ruslan Sharafutdinov <r.sharafutdinov@corp.vk.com>
2021-09-15 11:55:39 +0300

    add setuptolls files

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from os.path import join, dirname
 
 
 setup(
     name='test_package',
     version='1.0',
-    packages=['test_package'],
+    packages=find_packages(where="test_package"),
+    package_dir={'': 'test_package'},
     test_suite='tests',
     long_description=open(join(dirname(__file__), 'README.txt')).read(),
 )
