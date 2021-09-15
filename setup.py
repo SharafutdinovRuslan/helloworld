bf2d0d4101dbe475c8a34aca4acb4960e077fd67
Ruslan Sharafutdinov <r.sharafutdinov@corp.vk.com>
2021-09-15 12:12:29 +0300

    refactor project structure

--- setup.py
+++ setup.py
@@ -3,10 +3,13 @@ from os.path import join, dirname
 
 
 setup(
-    name='test_package',
+    name='helloworld',
     version='1.0',
-    packages=find_packages(where="test_package"),
-    package_dir={'': 'test_package'},
-    test_suite='tests',
+    description='Setuptools example project',
     long_description=open(join(dirname(__file__), 'README.txt')).read(),
+    packages=find_packages(where="test_package"),
+    package_dir={'test_package': 'test_package'},
+    include_package_data=True,
+    zip_safe=False,
+    test_suite='tests',
 )
